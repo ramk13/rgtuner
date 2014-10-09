6dc1a6e0d476b060fd9e9a00716dfa458a0b359f
R K <ramk13@gmail.com>
2014-10-09 15:29:44 -0700

    Initial Mod of Spferical's rgtuner

--- README.md
+++ README.md
@@ -1,26 +1,105 @@
 rgtuner
 =======
 
-rgtuner is a program to 'tune' bots for robotgame.org by modifying one variable
-at a time, throwing versions of a bot with different possible values for the
-variable against each other and seeing which comes out on top.
+Credit:
+Originally forked from https://github.com/Spferical/rgtuner
 
-Included is an example bot, a version of Sfpar. All of the globals are added at
-the beginning of the file. This program is what I used to create Sfpar II: I
-merely optimized the variables in it.
+rgtuner is a program to 'tune' bots for robotgame.org by modifying one variable at a time, throwing versions of a bot with different possible values for the variable against specified bots. rgtuner is not an optimizer in the usual definition. It runs a matrix of variants of the original file and scores each variant by average scoring margin. 
 
-rgtuner can't necessarily be used to find the optimum behavior for your bot as
-it uses a greedy algorithm, but the 'tuned' version of the bot should perform
-slightly better against the old one.
+Because of the variance present in Robot Game you'll need to run a decent number of matches (minimum 10, probably closer to 25) for each case to get a reasonable sample. Even with that many matches there is still significant variance in scoring margin. Depending on the options you choose, the tuner can take a VERY long time to run. It gives an indication of this as it's running.
+
+Included is an example bot, a version of Sfpar. 
 
 #Requirements
 - Python
 - rgkit
 
 #Usage
-`$ python rgtuner.py [-h] [-p PROCESSES] constant file enemyfile`
+`$ python rgtuner.py [-h] [-p PROCESSES] file_to_optimize`
 
-e.g.
-`$ python rgtuner.py -p 6 SURROUND_WEIGHT sfpari.py stupid.py`
-to optimize the SURROUND_WEIGHT variable in sfpari.py against stupid.py, running comparisons in 6
-different processes.
+#In file syntax
+Variables to optimize are marked by `#optimize (parameters)` in the line above the value assignment. There are three ways to specify the range of optimization values:
+
+1 - a list of values to use directly:
+```python
+#optimize = list: 3, 5, 10, 20
+opt_variable = 10
+```
+
+2 - evenly spaced steps around the original value by multiplication, the multiplier gives the min/max value
+```python
+#optimize = multsteps: 2, 5
+opt_variable = 10
+```
+is the same as `list: 5, 7.07, 10, 14.14, 20`
+
+3 - evenly spaced steps around the original value by addition, the adder gives the min/max value
+```python
+#optimize = addsteps: 5, 3
+opt_variable = 10
+```
+is the same as `list:  5,  10,  15`
+
+Enemy robots to match against are listed with `#enemy_file = path_to_enemy_robot`
+If no robot is given, the original robot is used
+
+Other options include:
+Number of matches to run per enemy robot:
+```python
+#num_matches = xxx
+```
+Path to log file (mirrors std_out). If none is given, stdout isn't logged.
+```python
+#log_file = path_to_log_file
+```
+Whether to save the best value in the original file. If the option is not specified the default is to overwrite the original file.
+```python
+#save_optimum = 0
+```
+
+example:
+
+```python
+#num_matches = 3
+#save_optimum = 0
+#enemy_file = liquid1.0.py
+#enemy_file = RageMK1.py
+
+#optimize = addsteps: 5, 5
+COULD_DIE_WEIGHT = 20.0
+#optimize = list: 30, 34, 36, 38
+ENEMY_IN_LOC_WEIGHT = 34.25
+#optimize = multsteps: 2, 3
+SURROUND_WEIGHT = 0.5
+```
+
+example output:
+```
+Starting optimization at 15:04:23.
+Running 5*3*4 = 60 test cases of 3 matches (180 total) each over 3 processes
+COULD_DIE_WEIGHT -  15.00  17.50  20.00  22.50  25.00
+SURROUND_WEIGHT -   0.25   0.50   1.00
+ENEMY_IN_LOC_WEIGHT -  30.00  34.00  36.00  38.00
+
+           liquid1.0 -   3: [...       ] -    [2, 1, 0] by [43, 38] diff   5.7 std  6.1 in   6.6s
+             RageMK1 -   3: [...       ] -    [3, 0, 0] by [44, 25] diff  19.0 std  4.0 in   6.4s
+ 15.00   0.25  30.00                - avg:  12.3 std:  8.6 in  13.1s. (   1/  60) Done at 15:17:17
+           liquid1.0 -   3: [...       ] -    [2, 1, 0] by [42, 39] diff   3.0 std  6.2 in   7.3s
+             RageMK1 -   3: [...       ] -    [3, 0, 0] by [42, 31] diff  11.3 std  0.6 in   7.5s
+ 15.00   0.25  34.00                - avg:   7.2 std:  6.0 in  14.9s. (   2/  60) Done at 15:18:09
+           liquid1.0 -   3: [...       ] -    [2, 1, 0] by [42, 41] diff   0.7 std  3.2 in   7.0s
+             RageMK1 -   3: [...       ] -    [3, 0, 0] by [42, 30] diff  12.0 std  7.0 in   6.7s
+...
+
+Final Results in  758.0s at 16:12:37
+Best Result:
+ 15.00   0.50  30.00                - avg:  13.8 std:  7.9
+
+Sorted Results:
+ 15.00   0.50  30.00                - avg:  13.8 std:  7.9
+ 15.00   0.25  30.00                - avg:  12.3 std:  8.6
+ 17.50   0.25  30.00                - avg:  11.2 std:  6.7
+ 17.50   0.25  34.00                - avg:  10.8 std:  5.9
+ 25.00   0.50  36.00                - avg:  10.7 std:  1.9
+...
+```
--- rgtuner.py
+++ rgtuner.py
@@ -1,225 +1,343 @@
 #!/usr/bin/env python2
-from __future__ import print_function
 import os
+import sys
 import multiprocessing
 import re
 import shutil
 import argparse
-filesRemaining = []
-botScores = {}
 import random
+import time
+import itertools
+from math import sqrt, exp
 from rgkit.run import Runner, Options
 from rgkit.settings import settings as default_settings
-def make_variants(variable, robot_file, possibilities):
-    """Makes variants of the file robot_file  with the constant variable
-    changed for each possibility.
 
-    e.g. if the variable is "ELEPHANTS" and the possibilities are [1, 2, 3],
-    this will find the line
-    ELEPHANTS = 3
-    in robobt_file and make a copy of the file for each value in possibilities.
-    1.py will have ELEPHANTS = 1, 2.py will have ELEPHANTS = 2, etc.
+"""
+example:
 
-    Raises IndexError if the variable name is not found in the file robot_file.
-    The line assigning the constant variable must be the first line in that
-    file has the variable name in it.
+#optimize = list: 8, 10, 12
+minimum_hp = 10
+#optimize = multsteps: 2, 5
+bad_guy_score = 20
+#enemy_file = ./robot_library/liquid1.0.py
+#enemy_file = ./robot_library/SfparI.py
+#enemy_file = ./robot_library/RageMk1.py
+#num_matches = 25
+#log_file = logfile.txt
+#save_optimum = 1
+"""
+
+# less_than_acceptance = 0.6
+
+def meanstd(x):
     """
-    filenames = []
+    Modified from:
+    https://www.physics.rutgers.edu/~masud/computing/WPark_recipes_in_python.html
+    """
+    n, mean, std = len(x), 0, 0
+    for a in x:
+	mean = mean + a
+    mean = mean / float(n)
+    for a in x:
+	std = std + (a - mean)**2
+    std = sqrt(std / float(n-1))
+    return mean, std
+    
+def erfcc(x):
+    """
+    Complementary error function.
+    Modified with normcdf and normpdf from:
+    http://stackoverflow.com/questions/809362/cumulative-normal-distribution-in-python
+    """
+    z = abs(x)
+    t = 1. / (1. + 0.5*z)
+    r = t * exp(-z*z-1.26551223+t*(1.00002368+t*(.37409196+
+    	t*(.09678418+t*(-.18628806+t*(.27886807+
+    	t*(-1.13520398+t*(1.48851587+t*(-.82215223+
+    	t*.17087277)))))))))
+    if (x >= 0.):
+    	return r
+    else:
+    	return 2. - r
+
+def normcdf(x, mu, sigma):
+    t = x-mu;
+    y = 0.5*erfcc(-t/(sigma*sqrt(2.0)));
+    if y>1.0:
+        y = 1.0;
+    return y
+
+def normpdf(x, mu, sigma):
+    u = (x-mu)/abs(sigma)
+    y = (1/(sqrt(2*pi)*abs(sigma)))*exp(-u*u/2)
+    return y
+    
+def linspace(start, stop, n):
+    """
+    Modified from:
+    http://stackoverflow.com/questions/12334442/does-python-have-a-linspace-function-in-its-std-lib
+    """
+    if n < 2:
+        yield stop
+        return
+    h = (stop - start) / float(n - 1)
+    for i in range(n):
+        yield start + h * i
+
+class Logger(object):
+    def __init__(self, filename="default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        
+def make_variant(variables, robot_file, outfile='lastopt'):
+    """Makes a variant of the file robot_file with each variable
+    listed as a key in 'variables' changed to the value in 'variables'
+    """
     with open(robot_file, 'r') as f:
         lines = f.readlines()
-       
-        for i, line in enumerate(lines):
-          if variable in line:
-            break
-        assert '=' in line
-        for p in possibilities:
-            varandp = variable + str(p)
-            lines[i] = "%s = %s\n" % (variable, p)
-            filenames.append(varandp)
-            with open(varandp, 'w') as pfile:
-                for line in lines:
-                    pfile.write(line)
+        for varname in variables:
+            for i, line in enumerate(lines):
+                if varname in line: 
+                    break
+            assert '=' in line
+            lines[i] = "%s = %s\n" % (varname, variables[varname])
+    with open(outfile, 'w') as pfile:
+        for line in lines:
+            pfile.write(line)
 
-    return filenames
+    return outfile
 
-
-def get_current_value(variable, robot_file):
-    """
-    Returns the value of the constant variable in the robot file.
-
-    This function finds the first line in the file robot_file that has the
-    variable name in it, and parses the value after the '=' in that line for a
-    float, returning it.
-
-    Raises IndexError if the variable name is not found in the file robot_file.
-    The line assigning the constant variable must be the first line in that
-    file has the variable name in it.
-    """
+def get_variables_and_params(robot_file):
+    variables = {}
+    enemy_files = []
+    opts = {'num_matches' : 25, 'log_file' : None, 'save_optimum' : 0}
+    
+    params = {}
     with open(robot_file, 'r') as f:
-        for i, line in enumerate(f):
-          if variable in line:
-            break
-        assert '=' in line
-    return float(line[line.index('=') + 1:])
+        all_lines = f.readlines()
+        for i, line in enumerate(all_lines):
+            for opt in opts.iterkeys():
+                if ('#' + opt) in line:
+                    try:
+                        opts[opt] = float(line[line.index('=') + 1:])
+                    except ValueError:
+                        opts[opt] = line[line.index('=') + 1:].strip()
+                    
+            if '#enemy_file' in line:
+                var_name = line[line.index('=')+1:].strip()
+                if os.path.isfile(var_name):
+                    enemy_files.append(var_name)
+                else:
+                    raise IOError(var_name + ' not found')
+            elif '#optimize' in line:
+                next_line = all_lines[i+1]
+                assert '=' in next_line
+                var_name = next_line[:next_line.index('=')].strip()
+                orig_val = float(next_line[next_line.index('=') + 1:])
+                
+                opt_param = line[line.index('='):].strip()
+                if 'list' in opt_param:
+                    var_values = [float(x) for x in opt_param[opt_param.index(':')+1:].strip().split(',')]
+                elif 'multsteps' in opt_param:
+                    opt_values = [float(x) for x in opt_param[opt_param.index(':')+1:].strip().split(',')]
+                    var_values = [orig_val*opt_values[0]**x for x in linspace(-1,1,int(opt_values[1]))]
+                elif 'addsteps' in opt_param:
+                    opt_values = [float(x) for x in opt_param[opt_param.index(':')+1:].strip().split(',')]
+                    var_values = list(linspace(orig_val-opt_values[0],orig_val+opt_values[0],int(opt_values[1])))
+                else:
+                    raise Exception("No optimization options given on line %i" % i)
+                variables[var_name] = var_values
+        
+    if not enemy_files:
+        enemy_files.append(robot_file)
+    return variables, enemy_files, opts
 
+def run_match(bot1, bot2):
+    """Runs a match between two robot files."""
 
-def optimize_variable(precisionParam, matchNum, enemies, variable, robot_file, processes):
-    pool = multiprocessing.Pool(processes)
-    """
-    Creates a bunch of variants of the file robot_file, each with variable
-    changed, then runs a tournament between the variants to find the best one.
-    The file robot_fily is modified to contain the best value, and it is
-    returned.
-    """
-    base_value = get_current_value(variable, robot_file)
-
-    precision = precisionParam
-
-    while precision >= 0.1:
-        print('RUNNING WITH BASE VALUE', base_value, \
-                'PRECISION', precision)
-
-        values_to_test = [base_value - precision,
-            base_value + precision, base_value]
-
-        files = make_variants(variable, robot_file, values_to_test)
-        best_file = run_tourney(matchNum,enemies, files, pool)
-        best_value = values_to_test[files.index(best_file)]
-        if best_value == base_value:
-            precision /= 2.0
-            print('best value remains', best_value)
-            print('decreasing precision to', precision)
-        else:
-            base_value = best_value
-            print('new \'best\' value is', best_value)
-
-    shutil.copy(make_variants(variable, robot_file, [base_value])[0],
-                robot_file)
-
-    return base_value
-
-def run_match(bot1, bot2):   
-    #rgkit integration
     runner = Runner(player_files=(bot1,bot2), options=Options(quiet=4, game_seed=random.randint(0, default_settings.max_seed)))
     scores0, scores1 = runner.run()[0]
-    if scores0 > scores1:
-      return (scores0, scores1, scores0 - scores1, bot1)
-    elif scores1 > scores0:
-      return (scores0, scores1, scores1 - scores0, bot2)
-    else:
-      return (scores0, scores1, 0,'tie')
+    return [scores0, scores1]
 
-
-def versus(matchNum,bot1, bot2, pool):
+def versus(bot1, bot2, num_matches, cpu_count):
     """Launches a multithreaded comparison between two robot files.
     run_match() is run in separate processes, one for each CPU core, until 100
     matches are run.
     Returns the winner, or 'tie' if there was no winner."""
-    bot1Score = 0
-    bot2Score = 0
+    W, L, D = 0, 0, 0
+    tot_scores = [0, 0]
+    avg_scores = [0, 0]
+    all_scores = []
+    show_match_results = 1
 
-    matches_to_run = matchNum
+    pool = multiprocessing.Pool(cpu_count)
 
     try:
+        bot1name = os.path.splitext(os.path.basename(bot1))[0]
+        bot2name = os.path.splitext(os.path.basename(bot2))[0]
+        start_time = time.time()
         results = [pool.apply_async(run_match, (bot1, bot2))
-                   for i in xrange(matches_to_run)]
+                   for i in range(num_matches)]
+                   
+        if show_match_results:
+            sys.stdout.write('%20s - %3i: [' % (bot2name, num_matches))
+
+        tenths_done = 0
         for r in results:
-            s0, s1, s2, s3 = r.get(timeout=120)
-            print('battle result:',s3, ' difference:', s2)
-            bot1Score += s0
-            bot2Score += s1
+            scores = r.get(timeout=50)
+            if scores[0] > scores[1]:
+                W += 1
+            elif scores[0] < scores[1]:
+                L += 1
+            elif scores[0] == scores[1]:
+                D += 1
+            tot_scores = [tot_scores[x]+scores[x] for x in range(2)]
+            avg_scores = [tot_scores[x]//sum([W,L,D]) for x in range(2)]
+            all_scores.append(scores[0:2])
+            if (sum([W,L,D])*10//num_matches) > tenths_done:
+                tenths_done += 1
+                if show_match_results:
+                    sys.stdout.write ('.')
 
-            #otherwise, it's a tie, but we can ignore it
-
-        print('overall:', bot1, bot1Score, ':', bot2Score, bot2)
-        return bot1Score - bot2Score
-
-    except KeyboardInterrupt:
-        print('user did ctrl+c, ABORT EVERYTHING')
+        pool.close()
+        pool.join()
+        
+        if show_match_results:
+            diff = [s[0] - s[1] for s in all_scores]
+            avg, std = meanstd(diff)
+            sys.stdout.write (' '*(10-tenths_done))
+            print '] - %12s by %8s diff %5.1f std %4.1f in %5.1fs' % (str([W,L,D]), str(avg_scores), avg, std, time.time()-start_time)
+    except multiprocessing.TimeoutError:
+        if show_match_results:
+            sys.stdout.write ('x'*(10-tenths_done))
+            print '] - %12s by %8s in %5.1fs' % (str([W,L,D]), str(avg_scores), time.time()-start_time)
         pool.terminate()
-        for bot in filesRemaining:
-            os.remove(bot)
-        raise KeyboardInterrupt()
 
-def run_tourney(matchNum,enemies, botfiles, pool):
-    """Runs a tournament between all bot files in botfiles.
-    Returns the winner of the tournament."""
-    bestWin = ['', -5000]
-    scores = {}
-    for bot1 in botfiles:
-        filesRemaining.append(bot1)
-        scores[bot1] = 0
-    for enemy in enemies:
-        for bot1 in botfiles:
-            if bot1 in botScores[enemy] and botScores[enemy][bot1] != 0:
-                winScore = botScores[enemy][bot1]
-                print('ALREADY SCORED',str(bot1))
-            else:
-                winScore = versus(matchNum,bot1, enemy, pool)
-                botScores[enemy][bot1] = winScore
-            while winScore == 0:
-                print('VERSUS WAS A TIE. RETRYING...')
-                winScore = versus(matchNum,bot1, enemy, pool)
-                print('Difference in score:',str(bestWin[1]))
-            scores[bot1] += winScore
-        print(scores)
-    for bot1 in botfiles:
-        for bot2 in botfiles:
-            if bot1 != bot2 and scores[bot1] == scores[bot2]:
-                print("Two bots have same score, finding the winner")
-                bestWin[1] = versus(matchNum,bot1, bot2, pool)
-                while bestWin[1] == 0:
-                    print("Wow. Another Tie.")
-                    bestWin[1] = versus(matchNum,bot1, bot2, pool)
-                if bestWin[1] < 0:
-                    bestWin[0] = bot2
-                elif bestWin[1] > 0:
-                    bestWin[0] = bot1
-                else:
-                    print("WTF? Impossible Tie.")
-            elif scores[bot1] > bestWin[1]:
-                bestWin[1] = scores[bot1]
-                bestWin[0] = bot1
+    return [W,L,D], tot_scores, all_scores
 
-    for bf in botfiles:
-        if not bf == bestWin[0]:
-            print('removing',bf)
-            os.remove(bf)
-            filesRemaining.remove(bf)
-    print('Best Score:',str(bestWin[1]))
-    return bestWin[0]
+def get_netavg (values, var_names, robot_file, enemy_files, num_matches, cpu_count):
+    """Runs a robot_file against each enemy file for num_matches and returns
+    the average difference in score"""
+    total_matches = 0
+    total_all_scores = []
+    
+    variables = dict(zip(var_names, list(values)))
+    robot_to_eval = make_variant(variables, robot_file)
+    
+    for enemy in enemy_files:
+        wld, avg_scores, all_scores = versus(robot_to_eval, enemy, num_matches, cpu_count)
+        total_matches += sum(wld)
+        total_all_scores.extend(all_scores)
 
+    diff = [s[0] - s[1] for s in total_all_scores]
+    avg, std = meanstd(diff)
+    
+    os.remove(robot_to_eval)
+    
+    return avg, std
+
+class test_result():
+    def __init__(self, vars, avg, std):
+        self.vars = vars
+        self.avg = avg
+        self.std = std
+        
+    def __repr__(self):
+        return ('%-35s - avg: %5.1f std: %4.1f' % (' '.join(["%6.2f" % i for i in self.vars]), self.avg, self.std))
+        
+    def __lt__(self, other):
+        newavg = self.avg - other.avg
+        newstd = sqrt(self.std**2 + other.std**2)
+        return self.avg < other.avg
+        # return normcdf((-newavg/newstd),0,1) > less_than_acceptance
+    
+def optimize_variables(robot_file, cpu_count):
+    variables, enemy_files, opts = get_variables_and_params(robot_file)
+    
+    if opts['log_file'] is not None:
+        sys.stdout = Logger(opts['log_file'])
+
+    var_names = variables.keys()
+    value_list = variables.values()
+    
+    num_matches = int(opts['num_matches'])
+    args_ = (var_names, robot_file, enemy_files, num_matches, cpu_count)
+
+    test_ranges = []
+    for val in value_list:
+        test_ranges.append(val)
+        
+    test_cases = list(itertools.product(*test_ranges))
+    
+    start_time = time.time()
+    total_cases = len(test_cases)
+    remaining_cases = total_cases
+    case_mult = '*'.join([str(len(x)) for x in test_ranges])
+    
+    print 'Starting optimization at %s.\nRunning %s = %i test cases of %i matches (%i total) each over %i processes' % (time.strftime("%H:%M:%S",time.localtime(start_time)),case_mult,remaining_cases, num_matches, remaining_cases*num_matches, cpu_count)
+    
+    for i in range(len(value_list)):
+        print '%15s - %s' % (var_names[i], ' '.join(["%6.2f" % i for i in test_ranges[i]]))
+
+    print
+
+    test_results = []
+    for case in test_cases:
+        case_start = time.time()
+        res = test_result(case, *get_netavg(case, *args_))
+        test_results.append(res)
+        remaining_cases -= 1
+        avg_case_time = (time.time() - start_time)/float(total_cases-remaining_cases)
+        print '%s in %5.1fs. (%4i/%4i) Done at %s' % (str(res), time.time()-case_start, total_cases-remaining_cases, total_cases, time.strftime("%H:%M:%S",time.localtime(case_start+(avg_case_time)*remaining_cases)))
+        
+    # test_results.sort(key=lambda x: x.avg, reverse=True)
+    test_results.sort(reverse=True)
+    
+    print 'Final Results in %6.1fs at %s' % (time.time() - start_time, time.strftime("%H:%M:%S",time.localtime(time.time() - start_time)))
+    print 'Best Result:'
+    print test_results[0]
+    print
+    print 'Sorted Results:'
+    
+    for res in test_results:
+        print res
+    
+    if opts['save_optimum']:
+        new_variables = dict(zip(var_names, list(test_results[0].vars)))
+        make_variant(new_variables, robot_file, robot_file)
+
+    # return test_results[0].vars
 
 def main():
     
     parser = argparse.ArgumentParser(
-        description="Optimize constant values for robotgame.")
-    parser.add_argument(
-        "constant", type=str, help='The constant name to optimize.')
+        description="Optimize constant values for robotgame. See source for syntax in file")
+    # parser.add_argument(
+        # "constant", type=str, help='The constant name to optimize.')
     parser.add_argument(
         "file", type=str, help='The file of the robot to optimize.')
-    parser.add_argument(
-        "enemies", type=str, help='A comma-separated list of the enemy files.')
-    parser.add_argument(
-        "-pr", "--precision",
-        default=8.0,
-        type=float, help='The precision to start adjusting values at')
-    parser.add_argument(
-        "-m", "--matches",
-        default=100,
-        type=int, help='The number of matches to run per tourney')
+    # parser.add_argument(
+        # "enemies", type=str, help='A comma-separated list of the enemy files.')
+    # parser.add_argument(
+        # "-pr", "--precision",
+        # default=0.1,
+        # type=float, help='The fractional precision to start adjusting values at')
+    # parser.add_argument(
+        # "-m", "--matches",
+        # default=100,
+        # type=int, help='The number of matches to run per tourney')
     parser.add_argument(
         "-p", "--processes",
         default=multiprocessing.cpu_count(),
         type=int, help='The number of processes to simulate in')
-    args = vars(parser.parse_args())
-    eList = args['enemies'].split(',')
-    for e in eList:
-        botScores[e] = {}
-    best_value = optimize_variable(args['precision'],args['matches'],eList,
-        args['constant'], args['file'], processes=args['processes'])
-    print(best_value)
-
-
+    args = parser.parse_args()
+    
+    if os.path.isfile(args.file):
+        optimize_variables(args.file, args.processes)
+        
 if __name__ == '__main__':
     main()
